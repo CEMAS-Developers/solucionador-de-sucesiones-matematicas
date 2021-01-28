0fcb9bbeb681489c672acf33dbf0e9a1daa34428
Adonys <adonyssantoscemas@gmail.com>
2021-01-28 08:39:01 -0400

    Estructrura base del algoritmo

---
+++ index.js
@@ -0,0 +1,28 @@
+let aN = [3, 8, 13, 18];
+let index = 0;
+let aritmetica, geometrica; // Aritmetica y geometrica
+let result = 0;
+
+function sucecion () {
+	let result;
+
+	for (let i = 0; i < aN.length; i++) {
+		if (// a - b & b - c & c - d son iguales entonces es geometrica) 
+		{
+			//code (formula)	
+		} else {
+			for (let i = 0; i < aN.length; i++) {
+				if ((// a / b & b / c & c / d son iguales entonces es aritmetica) 
+				{
+					//code (formula)	
+				} 
+			}
+		} else {
+			// no se puede resolver
+		}
+	}
+
+	return result;
+}
+
+console.log(result);
