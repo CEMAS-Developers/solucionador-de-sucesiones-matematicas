25edc429f5621396346c8ae981ee40d1b8a0cc56
Adonys <adonyssantoscemas@gmail.com>
2021-01-29 08:27:05 -0400

    Solucion de sucesion aritmetica agregada

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-let aN = [3, 8, 13, 18];
+let a = [2, 7, 12];
 let index = 0;
 let aritmetica, geometrica; // Aritmetica y geometrica
 let result = 0;
 
-function sucecion () {
-	let result;
-
-	for (let i = 0; i < aN.length; i++) {
-		if (// a - b & b - c & c - d son iguales entonces es geometrica) 
-		{
-			//code (formula)	
-		} else {
-			for (let i = 0; i < aN.length; i++) {
-				if ((// a / b & b / c & c / d son iguales entonces es aritmetica) 
-				{
-					//code (formula)	
-				} 
-			}
-		} else {
-			// no se puede resolver
-		}
-	}
-
-	return result;
+function inAscOrder(arr) {
+  return (
+    arr.join("") ===
+    arr
+      .sort(function (a, b) {
+        return a - b;
+      })
+      .join("")
+  );
 }
 
-console.log(result);
+function sucesion(a, x) {
+  if (inAscOrder(a)) {
+    if (a[2] - a[1] === a[1] - a[0]) {
+      console.log("Es aritmetica");
+      let d = a[2] - a[1];
+      console.log(d);
+      console.log(a[0] + (x - 1) * d);
+    } else if (a[2] / a[1] === a[2] / a[1]) {
+      console.log("Es logaritmica");
+    } else {
+      console.log("No tiene solucion");
+    }
+  } else {
+    console.log("No tiene solucion");
+  }
+
+  return;
+}
+
+sucesion(a, 8);
+
+//console.log(result);
