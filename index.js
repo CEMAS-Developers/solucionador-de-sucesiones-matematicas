ba1f5b04ee7ae5b3b5041451078985d5a5894fcd
Adonys <adonyssantoscemas@gmail.com>
2021-01-30 11:05:40 -0400

    Funcion para progresiones aritmeticas agregada

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let a = [2, 7, 12];
-let index = 0;
-let aritmetica, geometrica; // Aritmetica y geometrica
-let result = 0;
+let a = [1, 5, 9];
+let n = 9;
+console.log(sucesion(a, n));
+
+function sucesion(a, n) {
+  if (inAscOrder(a)) return An(a, n);
+  else return "NS";
+}
 
 function inAscOrder(arr) {
   return (
@@ -14,25 +18,17 @@ function inAscOrder(arr) {
   );
 }
 
-function sucesion(a, x) {
-  if (inAscOrder(a)) {
-    if (a[2] - a[1] === a[1] - a[0]) {
-      console.log("Es aritmetica");
-      let d = a[2] - a[1];
-      console.log(d);
-      console.log(a[0] + (x - 1) * d);
-    } else if (a[2] / a[1] === a[2] / a[1]) {
-      console.log("Es logaritmica");
-    } else {
-      console.log("No tiene solucion");
-    }
-  } else {
-    console.log("No tiene solucion");
-  }
-
-  return;
+function An(a, n) {
+  if (a[2] - a[1] === a[1] - a[0]) {
+    let d = a[2] - a[1];
+    let An = a[0] + (n - 1) * d;
+    let Sn = (n * (a[0] + a[n - 1])) / 2;
+    return (
+      "A= " + a + ", D= " + d + ", A" + n + "= " + An + ", S" + n + "= " + Sn
+    );
+  } else if (a[2] / a[1] === a[2] / a[1]) {
+    // let r = a[2] / a[1];
+    // n = Math.pow(a[0] * r, n - 1);
+    return "No Code.";
+  } else return "NS";
 }
-
-sucesion(a, 8);
-
-//console.log(result);
