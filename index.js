2d0918ef5a6ab85cf086454c8d2291df2fd5b4b5
Adonys <adonyssantoscemas@gmail.com>
2021-01-30 14:27:02 -0400

    Solucion de errores

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ const An = (a, n) => {
     let d = a[2] - a[1];
     let An = a[0] + (n - 1) * d;
     let Sn = (n * (a[0] + An)) / 2;
-    let type = "geometrica";
+    let type = "aritmetica";
     let result = {
       A: a,
       D: d,
       Type: type,
       An: An,
-      Sn: sn,
+      Sn: Sn,
     };
     return result;
   } else if (a[2] / a[1] === a[2] / a[1] || a[2] * a[1] === a[2] * a[1]) {
     let r = a[2] / a[1];
     let An = a[0] * Math.pow(r, n - 1);
     let Sn = (An * r - a[0]) / (r - 1);
-    let type = "aritmetica";
+    let type = "geometrica";
     let result = {
       A: a,
       R: r,
