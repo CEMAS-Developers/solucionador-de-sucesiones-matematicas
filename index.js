db808f893ab36ff4b9188f24af508c95ef8d553e
Adonys <adonyssantoscemas@gmail.com>
2021-01-30 14:19:39 -0400

    Objeto resultado agregado

--- index.js
+++ index.js
@@ -14,16 +14,28 @@ const An = (a, n) => {
     let d = a[2] - a[1];
     let An = a[0] + (n - 1) * d;
     let Sn = (n * (a[0] + An)) / 2;
-    return (
-      "A=" + a + "; D=" + d + "; A" + n + "=" + An + "; S" + n + "=" + Sn + ";"
-    );
+    let type = "geometrica";
+    let result = {
+      A: a,
+      D: d,
+      Type: type,
+      An: An,
+      Sn: sn,
+    };
+    return result;
   } else if (a[2] / a[1] === a[2] / a[1] || a[2] * a[1] === a[2] * a[1]) {
     let r = a[2] / a[1];
     let An = a[0] * Math.pow(r, n - 1);
     let Sn = (An * r - a[0]) / (r - 1);
-    return (
-      "A=" + a + "; R=" + r + "; A" + n + "=" + An + "; S" + n + "=" + Sn + ";"
-    );
+    let type = "aritmetica";
+    let result = {
+      A: a,
+      R: r,
+      Type: type,
+      An: An,
+      Sn: sn,
+    };
+    return result;
   } else return "NS";
 };
 
